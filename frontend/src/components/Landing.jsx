22325815bc48d15c521cbbfac536d491fd9119a9
JAY-PATEL2004 <jaypatel15022004@gmail.com>
2024-10-08 14:10:08 +0530

    Quiz and Counsellor

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-pink-500">
-      <Link to="/counsellor" className="text-3xl text-white">
+      {/* <Link to="/counsellor" className="text-3xl text-white">
         Go to Counselor List
+      </Link> */}
+      <Link to="/quizWindow" className="text-3xl text-white">
+        Attempt Quiz
       </Link>
     </div>
   );
