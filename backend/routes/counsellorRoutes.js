4fc5d185256469765e7cc277f02a2ec72c7397d6
JAY-PATEL2004 <jaypatel15022004@gmail.com>
2024-10-14 16:52:04 +0530

    Consellor Reg

--- backend/routes/counsellorRoutes.js
+++ backend/routes/counsellorRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addNewSlot, changeReqStatus, getAllCounsInfo,getAllSlots,getInfoById } from "../controllers/counsellorController.js";
+import { addNewSlot, changeReqStatus, counsellorReg, getAllCounsInfo,getAllSlots,getInfoById } from "../controllers/counsellorController.js";
 
 const router=express.Router();
 
@@ -10,6 +10,7 @@ router.route("/getInfoById").get(getInfoById);
 router.route("/getAllSlots").get(getAllSlots);
 router.route("/addNewSlot").post(addNewSlot);
 router.route("/changeReqStatus").post(changeReqStatus);
+router.route("/counsellorReg").post(counsellorReg)
 
 
 
